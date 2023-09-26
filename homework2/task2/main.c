5f5c83e60c26a97a27cafe06b43d8822830406e3
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-26 19:59:09 +0300

    upload task2 now with the power less than 0

--- homework2/task2/main.c
+++ homework2/task2/main.c
@@ -1,10 +1,12 @@
+#include "../../lib/functions.h"
 #include <stdio.h>
+#include <math.h>
 
-int recursiveExponentiation(int number, int power)
+float recursiveExponentiation(int number, int power)
 {
     if (power < 0)
     {
-        return -1;
+        return 1 / recursiveExponentiation(number, -power);
     }
     if (power == 0)
     {
@@ -18,30 +20,79 @@ int recursiveExponentiation(int number, int power)
     return b * b;
 }
 
-int linearExponentiation(int number, int power)
+float linearExponentiation(int number, int power)
 {
-    if (power < 0)
-    {
-        return -1;
-    }
     int result = 1;
-    for (int i = 0; i < power; ++i)
+    for (int i = 0; i < abs(power); ++i)
     {
         result *= number;
     }
-    return result;
+    if (power >= 0)
+    {
+        return result;
+    }
+    return (float)1 / result;
+}
+
+int testLinearExponentiation(void) {
+    if (linearExponentiation(2, 3) != 8) return -1;
+    if (linearExponentiation(5, 2) != 25) return -2;
+
+    if (linearExponentiation(2, -3) != 0.125) return -3;
+    if (linearExponentiation(5, -2) != (float)0.04) return -4;
+
+    if (linearExponentiation(2, 0) != 1) return -5;
+    if (linearExponentiation(5, 0) != 1) return -6;
+
+    if (linearExponentiation(0, 3) != 0) return -7;
+
+    if (linearExponentiation(2, 1) != 2) return -8;
+    if (linearExponentiation(5, 1) != 5) return -9;
+
+    if (linearExponentiation(-2, 4) != 16) return -10;
+    if (linearExponentiation(-5, 2) != 25) return -11;
+
+    if (linearExponentiation(-2, 3) != -8) return -12;
+    if (linearExponentiation(-5, 3) != -125) return -13;
+    return 0;
+}
+
+
+int testRecursiveExponentiation(void) {
+    if (recursiveExponentiation(2, 3) != 8) return -1;
+    if (recursiveExponentiation(5, 2) != 25) return -2;
+
+    if (recursiveExponentiation(2, -3) != (float)0.125) return -3;
+    if (recursiveExponentiation(5, -2) != (float)0.04) return -4;
+
+    if (recursiveExponentiation(2, 0) != 1) return -5;
+    if (recursiveExponentiation(5, 0) != 1) return -6;
+
+    if (recursiveExponentiation(0, 3) != 0) return -7;
+
+    if (recursiveExponentiation(2, 1) != 2) return -8;
+    if (recursiveExponentiation(5, 1) != 5) return -9;
+
+    if (recursiveExponentiation(-2, 4) != 16) return -10;
+    if (recursiveExponentiation(-5, 2) != 25) return -11;
+
+    if (recursiveExponentiation(-2, 3) != -8) return -12;
+    if (recursiveExponentiation(-5, 3) != -125) return -13;
+    return 0;
 }
 
 int main()
 {
-    printf("Table of results for numbers from 2 to 7 raised to a power from 0 to 10\n\n\n");
-    for (int number = 2; number < 8; ++number)
+    if (testLinearExponentiation() != 0 || testRecursiveExponentiation() != 0)
     {
-        printf("Number %d\nLinear    Recursive\n", number);
-        for (int power = 0; power <= 10; ++power)
-        {
-            printf("%9d    %9d\n", linearExponentiation(number, power), recursiveExponentiation(number, power));
-        }
-        printf("\n\n");
+        return -1;
     }
+    printf("Enter a number and a power:\nnumber = ");
+    int number = getNum();
+    printf("power = ");
+    int power = getNum();
+
+    float resultLinear = linearExponentiation(number, power), resultRecursive = recursiveExponentiation(number, power);
+    printf("\nThe result of the linear algorithm: %f\n", resultLinear);
+    printf("\nThe result of the recursive algorithm: %f\n", resultRecursive);
 }
