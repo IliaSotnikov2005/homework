f5ae758c17f961d4d4d72db9358c0dfa469f622e
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-27 15:00:02 +0300

    bad bSearch realisation

--- homework3/task2/main.c
+++ homework3/task2/main.c
@@ -1,6 +1,40 @@
 #include "../../lib/functions.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+int compare(const void* a, const void* b)
+{
+    int int_a = *((int*)a);
+    int int_b = *((int*)b);
+
+    if (int_a == int_b) return 0;
+    else if (int_a < int_b) return -1;
+    else return 1;
+}
+
+int bSearch(const int number, const int *array, const int size)
+{
+    int attempt = 1;
+    int index = size / 2;
+    while (2 >> attempt <= size)
+    {
+        if (array[index] == number)
+        {
+            return index;
+        }
+        if (array[index] > number)
+        {
+            index /= 2;
+        }
+        else if (array[index] < number)
+        {
+            index *= 2;
+        }
+        ++attempt;
+    }
+    return -1;
+}
 
 int main()
 {
@@ -25,6 +59,19 @@ int main()
     randomIntArrayFill(numberArray, numbersCount);
 
     numberArray[0] = array[0];
+    qsort(array, size, sizeof(int), compare);
+    printIntArray(array, size);
+    printIntArray(numberArray, numbersCount);
 
-
+    for (int i = 0; i < numbersCount; ++i)
+    {
+        if (bSearch(numberArray[i], array, size))
+        {
+            printf("\n%d is in array\n", numberArray[i]);
+        }
+        else
+        {
+            printf("\n%d is not in array\n", numberArray[i]);
+        }
+    }
 }
