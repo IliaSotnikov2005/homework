691ef30a0f371e6ae71b348c81d179f7afb93609
ilia Sotnikov <ilia12st@yandex.ru>
2023-10-07 20:28:37 +0300

    almost done

--- homework4/task1/main.c
+++ homework4/task1/main.c
@@ -26,34 +26,23 @@ int getNum(void)
     return number;
 }
 
-void sumBin(int* bin1, int* bin2, int* result)
+void sumBin(int* bin1, int* bin2, int* result, const int size)
 {
-    int size1 = sizeof(bin1) / sizeof(int);
-    int size2 = sizeof(bin2) / sizeof(int);
-    int size3 = max(size1, size2);
-    int transfer = 0;
-    for (int i = size3; i > -1; --i)
+    int carry = 0;
+    for (int i = size - 1; i >= 0; --i)
     {
-        result[i] += transfer;
-        if (i - (i - size1) > -1)
-        {
-            result[i] += bin1[i - (i - size1)];
-        }
-        if (i - (i - size2) > -1)
-        {
-            result[i] += bin2[i - (i - size2)];
-        }
-        if (result > 1)
-        {
-            transfer = 1;
-        }
-        else
-        {
-            transfer = 0;
-        }
-        result[i] %= 2;
-        printf("\n%d %d %d\n", (bin1[i] + bin2[i]) % 2, result[i], i);
+        result[i] = (bin1[i] + bin2[i] + carry) % 2;
+        carry = (bin1[i] + bin2[i] + carry > 1) ? 1 : 0;
     }
+}
+
+void printBin(const int* bin, const int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d", bin[i]);
+    }
+    printf("\n");
 }
 
 void reverse(int* binNumber, int size)
@@ -70,29 +59,63 @@ void reverse(int* binNumber, int size)
     }
 }
 
-void toTwosComplement(int number, int *bin, int size)
+int binToDecimal(int* bin, int size)
 {
+    if (bin[0] == 1)
+    {
+        for (int i = size - 1; i >= 0; --i)
+        {
+            if (bin[i] == 1)
+            {
+                for (int j = i + 1; j < size; ++j)
+                {
+                    bin[j] = 1;
+                }
+                bin[i] = 0;
+                break;
+            }
+        }
+        for (int i = 0; i < size; ++i)
+        {
+            bin[i] = !bin[i];
+        }
+    }
+    int result = 0;
+    for (int i = 1; i <= size; ++i)
+    {
+        result += bin[size - i] * (2 << (i - 2));
+        printf("%d %d %d>", bin[size - i], i, 2 << (i - 2));
+    }
+    return result;
+}
+
+void toTwosComplement(int number, int *bin, int size)
+ {
     int i = 0;
-    int addition = (number < 0) ? 1: 0;
+    bool negative = number < 0;
     number = abs(number);
     while (number > 0)
     {
-        bin[i] = ((number % 2) + addition) % 2;
-        printf("^%d^", bin[i]);
+        bin[i] = number % 2;
         number /= 2;
         ++i;
     }
-    reverse(bin, size);
-}
-
-void printBin(const int* bin, const int size)
-{
-    printf("->%d<-", size);
-    for (int i = 0; i < size; ++i)
+    if (negative)
     {
-        printf("%d", bin[i]);
+        for (i = 0; i < size; ++i)
+        {
+            bin[i] = !bin[i];
+        }
+        int carry = 1;
+        for (i = 0; i < size; ++i)
+        {
+            bin[i] = bin[i] + carry;
+            carry = bin[i] / 2;
+            bin[i] %= 2;
+
+        }
     }
-    printf("\n");
+    reverse(bin, size);
 }
 
 int main()
@@ -101,29 +124,26 @@ int main()
 
     printf("Введите первое число:\n");
     int number1 = getNum();
-    int size1 = (int)floor(log2(abs(number1))) + 2;
+    int size1 = 8;
     int *binNumber1 = calloc(size1, sizeof(int));
     toTwosComplement(number1, binNumber1, size1);
-    reverse(binNumber1, size1);
     printf("\nПервое число в дополнительном коде: ");
     printBin(binNumber1, size1);
 
     printf("\nВведите второе число:\n");
     int number2 = getNum();
-    int size2 = (int)floor(log2(abs(number2))) + 2;
+    int size2 = 8;
     int *binNumber2 = calloc(size2, sizeof(int));
     toTwosComplement(number2, binNumber2, size2);
-    //reverse(binNumber2, size2);
+    printf("\nВторое число в дополнительном коде: ");
+    printBin(binNumber2, size2);
 
-    printBin(binNumber1, size1);
+    int size3 = 8;
+    int* binNumberSum = calloc(size3, sizeof(int));
+    sumBin(binNumber1, binNumber2, binNumberSum, size3);
+    printf("\nСумма двух чисел в бинарной системе:\n");
+    printBin(binNumberSum, size3);
 
-  
-    //int* binNumberSum = calloc(max(size1, size2) + 1, sizeof(int));
-    //sumBin(binNumber1, binNumber2, binNumberSum);
-    //printf("----------->");
-    //printBin(binNumber1);
-    //reverse(binNumber1, size1);
-    //printf("      ");
-   // printBin(binNumber1);
-    //printBin(binNumberSum);
+    int result = binToDecimal(binNumberSum, size3);
+    printf("\nВ десятичной системе: %d\n", result);
 }
