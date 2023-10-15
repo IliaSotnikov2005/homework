90605a0b2ce6944a6734175adb6df67e031faa0f
ilia Sotnikov <ilia12st@yandex.ru>
2023-10-16 00:56:02 +0300

    write test, rename NUMBER_OF_FIBONACCI_NUMBERS, add comments to the functions

--- homework2/task1/main.c
+++ homework2/task1/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
 
-#define NUMBER_OF_FIBONACCI_NUMBERS 40
+#define AMOUNT_OF_FIBONACCI_NUMBERS 40
 
+// recursively calculates the nth fibonacci number, returns -1 in case of incorrect argument
 int recursiveFibonacci(int number)
 {
     if (number < 1)
@@ -16,6 +18,7 @@ int recursiveFibonacci(int number)
     return recursiveFibonacci(number - 1) + recursiveFibonacci(number - 2);
 }
 
+// linearly calculates the nth fibonacci number, returns -1 in case of incorrect argument
 int linearFibonacci(int number)
 {
     int number1 = 1, number2 = 1, number3 = 2;
@@ -36,11 +39,79 @@ int linearFibonacci(int number)
     return number3;
 }
 
+void recursiveFibonacciTest(bool *testResult)
+{
+    int testCases[][2] = {
+        {-2, -1},
+        {0, -1},
+        {1, 1},
+        {2, 1},
+        {3, 2},
+        {4, 3},
+        {5, 5},
+        {6, 8},
+        {7, 13},
+        {8, 21}
+    };
+
+    for (int i = 0; i < 10; ++i)
+    {
+        int input = testCases[i][0];
+        int expectedOutput = testCases[i][1];
+        int actualOutput = recursiveFibonacci(input);
+
+        testResult[i] = (actualOutput == expectedOutput);
+    }
+}
+
+void linearFibonacciTest(bool* testResult)
+{
+    int testCases[][2] = {
+        {-2, -1},
+        {0, -1},
+        {1, 1},
+        {2, 1},
+        {3, 2},
+        {4, 3},
+        {5, 5},
+        {6, 8},
+        {7, 13},
+        {8, 21}
+    };
+
+    for (int i = 0; i < 10; ++i)
+    {
+        int input = testCases[i][0];
+        int expectedOutput = testCases[i][1];
+        int actualOutput = linearFibonacci(input);
+
+        testResult[i] = (actualOutput == expectedOutput);
+    }
+}
+
 int main()
 {
+    bool testResult[10] = { 0 };
+    linearFibonacciTest(testResult);
+    for (int i = 0; i < 10; ++i)
+    {
+        if (!testResult[i])
+        {
+            printf("LINEAR TEST %d FAILED\n", i + 1);
+        }
+    }
+    recursiveFibonacciTest(testResult);
+    for (int i = 0; i < 10; ++i)
+    {
+        if (!testResult[i])
+        {
+            printf("RECURSIVE TEST %d FAILED\n", i + 1);
+        }
+    }
+
     printf("The fibonacci sequence constructed by a linear algorithm:\n");
-    float runtimesLinear[NUMBER_OF_FIBONACCI_NUMBERS] = { 0 };
-    for (int number = 1; number < NUMBER_OF_FIBONACCI_NUMBERS + 1; ++number)
+    float runtimesLinear[AMOUNT_OF_FIBONACCI_NUMBERS] = { 0 };
+    for (int number = 1; number < AMOUNT_OF_FIBONACCI_NUMBERS + 1; ++number)
     {
         clock_t start = clock();
         printf("%d ", linearFibonacci(number));
@@ -51,8 +122,8 @@ int main()
     printf("\n\n");
 
     printf("The fibonacci sequence constructed by a recursive algorithm:\n");
-    float runtimesRecursive[NUMBER_OF_FIBONACCI_NUMBERS] = { 0 };
-    for (int number = 1; number < NUMBER_OF_FIBONACCI_NUMBERS + 1; ++number)
+    float runtimesRecursive[AMOUNT_OF_FIBONACCI_NUMBERS] = { 0 };
+    for (int number = 1; number < AMOUNT_OF_FIBONACCI_NUMBERS + 1; ++number)
     {
         clock_t start = clock();
         printf("%d ", recursiveFibonacci(number));
@@ -62,7 +133,7 @@ int main()
 
     printf("\n\n");
 
-    for (int i = 0; i < NUMBER_OF_FIBONACCI_NUMBERS; i++)
+    for (int i = 0; i < AMOUNT_OF_FIBONACCI_NUMBERS; i++)
     {
         float advantage = (runtimesRecursive[i] - runtimesLinear[i]) / CLOCKS_PER_SEC;
         if (advantage > 0)
