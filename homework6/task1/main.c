bd47505bb7167ab36eb37b10f407447f0d7f507d
ilia Sotnikov <ilia12st@yandex.ru>
2023-10-18 23:39:33 +0300

    write tests, finish the functionality

--- homework6/task1/main.c
+++ homework6/task1/main.c
@@ -1,24 +1,25 @@
 #include "../../lib/stack.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main()
+#define EXPRESSION_LENGTH 100
+
+int postfixCalculator(char *expression)
 {
     Stack* stack = NULL;
-    char symbol = 0;
     int operand1 = 0;
     int operand2 = 0;
     int result = 0;
 
-    while ((symbol = getchar()) != EOF)
+    int i = 0;
+    while (expression[i] != '\0')
     {
-        if (symbol == ' ')
+        char symbol = expression[i];
+        ++i;
+        if (symbol == ' ' || symbol == '\n')
         {
             continue;
-        }
-        if (symbol == '\n')
-        {
-            break;
         }
         if (symbol >= '0' && symbol <= '9')
         {
@@ -51,6 +52,7 @@ int main()
                 result = operand1 / operand2;
                 break;
             }
+
             default:
                 break;
             }
@@ -58,6 +60,7 @@ int main()
             push(&stack, result);
         }
     }
+
     while (stackSize(stack) > 1)
     {
         operand2 = pop(&stack);
@@ -65,7 +68,57 @@ int main()
         result = operand1 * operand2;
         push(&stack, result);
     }
-    printf("Result is %d", top(stack));
 
+    result = top(stack);
     freeStack(&stack);
+
+    return result;
+}
+
+void testCalculator(bool *testResults) {
+    char expressions[5][20] = {
+        "5 3 +",
+        "9 2 - 3 *",
+        "7 2 / 4 *",
+        "1 2 + 3 * 4 -",
+        "9 3 / 2 + 5 *"
+    };
+
+    if (postfixCalculator(expressions[0]) == 8) {
+        testResults[0] = true;
+    }
+
+    if (postfixCalculator(expressions[1]) == 21) {
+        testResults[1] = true;
+    }
+
+    if (postfixCalculator(expressions[2]) == 12) {
+        testResults[2] = true;
+    }
+
+    if (postfixCalculator(expressions[3]) == 5) {
+        testResults[3] = true;
+    }
+
+    if (postfixCalculator(expressions[4]) == 25) {
+        testResults[4] = true;
+    }
+}
+
+int main()
+{
+    bool testResults[5] = { 0 };
+    testCalculator(&testResults);
+    for (int i = 0; i < 5; ++i)
+    {
+        if (!testResults[i])
+        {
+            printf("TEST %d FAILED", i);
+        }
+    }
+
+    char expression[EXPRESSION_LENGTH] = { 0 };
+    fgets(expression, EXPRESSION_LENGTH, stdin);
+
+    printf("Result is %d", postfixCalculator(expression));
 }
