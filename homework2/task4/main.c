8c0f502ed5dcc97bca97340cd84179f94de4aaa4
ilia Sotnikov <ilia12st@yandex.ru>
2023-10-19 15:58:35 +0300

    write a test function, fixed the solution itself

--- homework2/task4/main.c
+++ homework2/task4/main.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define SIZE 100
 
-void distribute(int* array, int size)
+int* distribute(int* array, int size)
 {
+    if (size <= 1)
+    {
+        return;
+    }
     int pivot = array[0];
     int pIndex = 0, index = 0;
     for (int i = 1; i < size; ++i)
@@ -17,14 +22,16 @@ void distribute(int* array, int size)
             array[pIndex] = array[i];
             array[i] = temp;
             ++pIndex;
+            index = i;
         }
     }
     int temp = array[pIndex];
     array[pIndex] = pivot;
-    array[pIndex + 1] = temp;
+    array[index] = temp;
+    return;
 }
 
-int checkArray(int* array, int size, int number)
+bool checkArray(int* array, int size, int number)
 {
     bool isGreater = false;
     for (int i = 0; i < size; ++i)
@@ -35,14 +42,39 @@ int checkArray(int* array, int size, int number)
         }
         if (isGreater != (array[i] >= number))
         {
-            return -1;
+            return false;
         }
     }
-    return 0;
+    return true;
+}
+
+void testDistribute(bool *testResult)
+{
+    int array1[] = { 5, 3, 8, 2, 9 };
+    distribute(array1, 5);
+    testResult[0] = checkArray(array1, 5, 5);
+
+    int array2[] = { 4, 4, 2, 6, 1, 5 };
+    distribute(array2, 6);
+    testResult[1] = checkArray(array2, 6, 4);
+
+    int array3[] = { 1 };
+    distribute(array3, 1);
+    testResult[2] = checkArray(array3, 1, 1);
 }
 
 int main()
 {
+    bool testResult[3] = { 0 };
+    testDistribute(testResult);
+    for (int i = 0; i < 3; ++i)
+    {
+        if (!testResult[i])
+        {
+            printf("TEST %d FAILED\n", i);
+        }
+    }
+
     int array[SIZE] = { 0 };
     randomIntArrayFill(array, SIZE);
     array[0] = rand();
@@ -56,10 +88,4 @@ int main()
     distribute(array, SIZE);
     printf("\nResult:\n");
     printIntArray(array, SIZE);
-
-    if (checkArray(array, SIZE, value) != 0)
-    {
-        printf("Array is incorrect\n");
-        return -1;
-    }
 }
