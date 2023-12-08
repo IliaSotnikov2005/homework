1f49685e889b79c223973f978dd0f4b8a1b49f19
ilia Sotnikov <ilia12st@yandex.ru>
2023-12-08 15:28:07 +0300

    add test

--- test2.2/task2/main.c
+++ test2.2/task2/main.c
@@ -3,13 +3,52 @@
 
 #include <stdio.h>
 
+bool test()
+{
+    Graph* graph = NULL;
+    int error = graphCreateFromFile("graphTest.txt", &graph);
+    if (error != graphOk)
+    {
+        return -1;
+    }
+    int** components = find—onnectivity—omponents(graph);
+    int expected[4][4] = { {1, 2, 3}, {4} };
+    for (size_t i = 0; i < graphNodesCount(graph); ++i)
+    {
+        for (size_t j = 0; j < graphNodesCount(graph); ++j)
+        {
+            if (components[i][j] != expected[i][j])
+            {
+                for (size_t k = i; k < graphNodesCount(graph); ++k)
+                {
+                    free(components[k]);
+                }
+                free(components);
+                return -2;
+            }
+        }
+        free(components[i]);
+    }
+    free(components);
+
+    graphFree(&graph);
+    return 0;
+}
+
 int main()
 {
+    if (test() != 0)
+    {
+        printf("TEST FAILED");
+        return -1;
+    }
+
     Graph* graph = NULL;
     int error = graphCreateFromFile("graph.txt", &graph);
     if (error != graphOk)
     {
-        printf("No %d", error);
+        printf("Error %d", error);
+        return -1;
     }
     int** components = find—onnectivity—omponents(graph);
     for (size_t i = 0; i < graphNodesCount(graph); ++i)
