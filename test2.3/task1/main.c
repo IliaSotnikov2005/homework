99f15cc08191c3ff7d00e416020afec271b40a43
ilia Sotnikov <ilia12st@yandex.ru>
2023-12-22 14:51:11 +0300

    write test

--- test2.3/task1/main.c
+++ test2.3/task1/main.c
@@ -2,7 +2,40 @@
 
 #include <stdio.h>
 
+int test(void)
+{
+    rewrite("test.txt", -4, 6);
+    int expected[] = { -12, -5, -6, 6, 5, 4, 3, 2, 1, 0, -4, 3, 4, 3, 10, 9, 8, 7, 7 };
+    FILE* output = fopen("output.txt", "r");
+    if (output == NULL)
+    {
+        return 1;
+    }
+     
+    int number = 0;
+    for (size_t i = 0; i < 19; ++i)
+    {
+        if (fscanf(output, "%d", &number) == -1)
+        {
+            return 2;
+        }
+        if (number != expected[i])
+        {
+            return i + 3;
+        }
+    }
+
+    return 0;
+}
+
 int main()
 {
-    rewrite("text.txt", 4, 10);
+    int errorCode = test();
+    if (errorCode != 0)
+    {
+        printf("test %d failed", errorCode);
+        return errorCode;
+    }
+
+    //rewrite("text.txt", 4, 10);
 }
