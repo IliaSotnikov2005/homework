ea7b2f5ed8b86648d74b2916aa4f7c48d55b7a9a
ilia Sotnikov <ilia12st@yandex.ru>
2023-12-26 19:42:40 +0300

    rewrite without tests

---
+++ homework7/task3/list.h
@@ -0,0 +1,30 @@
+#pragma once
+
+typedef enum sortedListErrorCode
+{
+    sortedListOk = 0,
+    sortedListMemoruAllocationError = -1,
+    sortedListNullPointerError = -2,
+    sortedListListIsEmptyError = -3,
+    sortedListNoSuchElementInList = -4
+} sortedListErrorCode;
+
+typedef struct SortedList SortedList;
+
+// Create list.
+SortedList* createList(void);
+
+// Add element to list.
+sortedListErrorCode sortedListPush(SortedList* list, const int value);
+
+// Remove list element by value.
+sortedListErrorCode sortedListRemove(SortedList* list, const int value);
+
+// Print list.
+sortedListErrorCode sortedListPrint(const SortedList* list);
+
+// Free list.
+sortedListErrorCode sortedListFree(SortedList** head);
+
+// List to array.
+int* sortedListToArray(const SortedList* head);
--- homework7/task3/main.c
+++ homework7/task3/main.c
@@ -1,114 +1,93 @@
-#include "../../lib/list.h"
+#include "list.h"
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
-bool checkResult(List* list, int* correct, const int size)
+enum ErrorCode
 {
-    for (int i = 0; i < size; ++i)
-    {
-        if (getNth(list, i) != correct[i])
-        { }
-    }
-}
-
-bool* test(bool* testResults)
-{
-    List* list = createList();
-    push(&list, 7);
-    int correct[] = { 7 };
-    int* listArray = toArray(list);
-    testResults[0] = memcmp(listArray, correct, sizeof(correct)) == 0;
-
-    push(&list, 1);
-    int correct1[] = {1, 7};
-    listArray = toArray(list);
-    testResults[1] = memcmp(listArray, correct1, sizeof(correct1)) == 0;
-
-    push(&list, 5);
-    int correct2[] = { 1, 5, 7 };
-    listArray = toArray(list);
-    testResults[2] = memcmp(listArray, correct2, sizeof(correct2)) == 0;
-
-    push(&list, 9);
-    int correct3[] = { 1, 5, 7, 9 };
-    listArray = toArray(list);
-    testResults[3] = memcmp(listArray, correct3, sizeof(correct3)) == 0;
-
-    deleteNth(&list, 1);
-    int correct4[] = { 1, 7, 9 };
-    listArray = toArray(list);
-    testResults[4] = memcmp(listArray, correct4, sizeof(correct4)) == 0;
-
-    deleteList(&list);
-}
+    ok = 0,
+    memoryAllocationError = -1,
+    invalidInput = -2
+};
 
 int main()
 {
-    bool testResults[5] = {0};
-    test(testResults);
-    for (int i = 0; i < 5; ++i)
+    // do tests
+
+    SortedList* list = createList();
+    if (list == NULL)
     {
-        if (!testResults[i])
-        {
-            printf("TEST %d FAILED\n", i);
-        }
+        printf("Memory allocation error");
+        return memoryAllocationError;
     }
 
-    List* list = createList();
-    printf("List of commands:\n0 - exit\n1 - add element to list\n2 - delete element\n3 - print list\n");
-    printf("Enter your command: ");
-    int number = 0;
-    if (scanf_s("%d", &number) == NULL)
+    printf("List of commands:\n"
+           "0 - exit\n"
+           "1 - add element to list\n"
+           "2 - delete element\n"
+           "3 - print list\n");
+
+    int command = -1;
+
+    while (command != 0)
     {
-        printf("Invalid input");
-        return -1;
-    }
-    while (number != 0)
-    {
-        switch (number)
+        printf("Enter your command: ");
+        if (scanf_s("%d", &command) == 0)
+        {
+            printf("Invalid input\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        switch (command)
         {
         case 1:
         {
+            printf("\nAdding a value to sorted list:\n");
             printf("\nEnter the value to add: ");
+
             int value = 0;
-            if (scanf_s("%d", &value) == NULL)
+            if (scanf_s("%d", &value) == 0)
             {
                 printf("Invalid input");
-                return -1;
+                return invalidInput;
             }
-            push(&list, value);
+
+            sortedListPush(list, value);
+
             break;
         }
         case 2:
         {
-            printf("\nEnter the index of the item to delete: ");
-            int index = 0;
-            if (scanf_s("%d", &index) == NULL)
+            printf("\nRemoving the value:\n");
+            printf("\nEnter the value to delete: ");
+
+            int value = 0;
+            if (scanf_s("%d", &value) == 0)
             {
-                return -1;
+                return invalidInput;
             }
-            deleteNth(&list, index);
+
+            sortedListRemove(list, value);
+
+            printf("\nSuccess\n");
             break;
         }
         case 3:
         {
-            printf("\nPrint the list: ");
-            printList(list);
+            printf("\nContents of the sorted list: ");
+            sortedListPrint(list);
             break;
         }
         default:
         {
+            printf("\nUnknown command\n");
             break;
         }
         }
-        printf("\nEnter your command: ");
-        if (scanf_s("%d", &number) == NULL)
-        {
-            printf("Invalid input");
-            return -1;
-        }
     }
-    deleteList(&list);
+
+    sortedListFree(&list);
     printf("\nGood bye");
 }
