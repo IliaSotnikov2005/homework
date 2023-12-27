7b725007696a2a87ce0a84af6203a4cb014c08fe
ilia Sotnikov <ilia12st@yandex.ru>
2023-12-27 12:45:06 +0300

    deleted lexer error codes

--- importantTest3.3/lexerOfEmails.h
+++ importantTest3.3/lexerOfEmails.h
@@ -2,11 +2,5 @@
 
 #include <stdbool.h>
 
-// Lexer error codes.
-typedef enum lexerErrorCode
-{
-    lexerOk = 0
-} lexerErrorCode;
-
 // Checks if given string is email address.
 bool isEmailAddress(const char* string);
